1373864c0076bfc0a000afbbc98e54d080eb3be4
Max Yezerskyi <m.yezerskyi@gmail.com>
2020-05-18 19:55:39 +0300

    Change name function)

--- PyTest_choice_browser_task/test_items.py
+++ PyTest_choice_browser_task/test_items.py
@@ -2,7 +2,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link_pass(browser):
+def test_add_button(browser):
     browser.get(link)
     time.sleep(30)
     buy_button = browser.find_element_by_css_selector(
